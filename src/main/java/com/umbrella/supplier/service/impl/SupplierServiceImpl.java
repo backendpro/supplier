332a165378c0d3d2552ec3d789583dc514fbe26d
backendpro <devcaiorodrigues@gmail.com>
2021-12-07 20:46:35 -0300

    removendo imports indesejados

--- src/main/java/com/umbrella/supplier/service/impl/SupplierServiceImpl.java
+++ src/main/java/com/umbrella/supplier/service/impl/SupplierServiceImpl.java
@@ -2,11 +2,9 @@ package com.umbrella.supplier.service.impl;
 
 import com.umbrella.supplier.model.request.SupplierRequest;
 import com.umbrella.supplier.model.response.SupplierResponse;
-import com.umbrella.supplier.persistence.repository.SupplierRepository;
 import com.umbrella.supplier.service.SupplierService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
